6c8bfd23a0af77edfa57078988c088d54162c7c0
Jonathan Hartman <j@p4nt5.com>
2015-09-28 14:17:01 -0700

    Initial commit; a freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Snoopy Cookbook CHANGELOG
+=========================
+
+v0.0.1 (2015-09-28)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'snoopy'
+maintainer       'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license          'apache2'
+description      'Installs/configures Snoopy Logger'
+long_description 'Installs/configures Snoopy Logger'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
