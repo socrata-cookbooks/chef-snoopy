33bb842387912b5dafe0f7b3e4941842fb14bf75
Jonathan Hartman <j@p4nt5.com>
2015-09-30 22:40:49 -0700

    Bump version for dev

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ maintainer_email 'jonathan.hartman@socrata.com'
 license          'apache2'
 description      'Installs/configures Snoopy Logger'
 long_description 'Installs/configures Snoopy Logger'
-version          '0.1.0'
+version          '0.1.1'
 
 supports         'ubuntu'
 # rubocop:enable SingleSpaceBeforeFirstArg
